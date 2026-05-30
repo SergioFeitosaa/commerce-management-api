db6247bceb2b6127321bc3694e73869f733e0b32
Sergio Feitosa <sergio.feitosa2@gmail.com>
2026-05-30 15:09:22 -0300

    feat(task-007): create customer service layer)

---
+++ src/main/java/br/com/sergio/customer_management_api/service/CustomerService.java
@@ -0,0 +1,33 @@
+package br.com.sergio.customer_management_api.service;
+
+import br.com.sergio.customer_management_api.dto.CustomerRequestDTO;
+import br.com.sergio.customer_management_api.dto.CustomerResponseDTO;
+import br.com.sergio.customer_management_api.entity.Customer;
+import br.com.sergio.customer_management_api.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerResponseDTO create(CustomerRequestDTO dto) {
+
+        Customer customer = new Customer();
+
+        customer.setName(dto.name());
+        customer.setEmail(dto.email());
+        customer.setCpf(dto.cpf());
+        customer.setPhoneNumber(dto.phoneNumber());
+
+        Customer customesaved = customerRepository.save(customer);
+            return new CustomerResponseDTO(
+                    customesaved.getId(),
+                    customesaved.getName(),
+                    customesaved.getEmail()
+            );
+    }
+
+}
